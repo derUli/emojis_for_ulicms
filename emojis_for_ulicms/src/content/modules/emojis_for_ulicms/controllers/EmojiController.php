d09b08320322becfee0ba7720ccdbc701dbf56aa
Ulrich Schmidt <support@ulicms.de>
2018-06-19 18:25:32 +0200

    Inkompatiblität mit DSGVO System korrigiert

--- emojis_for_ulicms/src/content/modules/emojis_for_ulicms/controllers/EmojiController.php
+++ emojis_for_ulicms/src/content/modules/emojis_for_ulicms/controllers/EmojiController.php
@@ -10,16 +10,28 @@ class EmojiController extends Controller {
 		$html = trim($input);
 		// Create a new DOM document
 		$dom = new DOMDocument();
-			// Parse the HTML. The @ is used to suppress any parsing errors
-			// that will be thrown if the $html string isn't valid XHTML.
-		@$dom->loadHTML('<?xml encoding="UTF-8">'.$html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+		// Parse the HTML. The @ is used to suppress any parsing errors
+		// that will be thrown if the $html string isn't valid XHTML.
+		@$dom->loadHTML('<?xml encoding="UTF-8">' . $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
-		$xpath = new DOMXPath($dom);
-		$textnodes = $xpath->query('//text()');
-		foreach($textnodes as $node) {
-			$node->nodeValue = emoji_unified_to_html($node->textContent);
+		$checkbox = $dom->getElementById(PrivacyCheckbox::CHECKBOX_NAME);
+		if($checkbox){
+			$oldValue = $checkbox->getAttribute("value");
+			$newValue = '##Check##';
+			$checkbox->setAttribute("value", $newValue);
 		}
-
-		return $dom->saveHTML();
+		$html = str_replace('<?xml encoding="UTF-8">', '', $dom->saveHTML($dom->documentElement));
+		
+		$html = emoji_unified_to_html($html);
+		
+		if($checkbox){
+			$dom = new DOMDocument();
+			@$dom->loadHTML('<?xml encoding="UTF-8">'.$html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+			$checkbox = $dom->getElementById(PrivacyCheckbox::CHECKBOX_NAME);
+			$checkbox->setAttribute("value", $oldValue);
+			$html = $dom->saveHTML($dom->documentElement);
+		}
+		$outputHtml = str_replace('<?xml encoding="UTF-8">', '', $html);
+		return $outputHtml;
 	}
 }
