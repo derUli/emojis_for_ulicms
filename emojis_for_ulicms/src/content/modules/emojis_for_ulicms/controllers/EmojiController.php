88b5adca1fac3d9bd869551711ed5011c1eac5eb
Ulrich Schmidt <u.schmidt@velian.de>
2018-06-19 16:14:48 +0200

    emojis in html-attributen werden nun ignoriert

--- emojis_for_ulicms/src/content/modules/emojis_for_ulicms/controllers/EmojiController.php
+++ emojis_for_ulicms/src/content/modules/emojis_for_ulicms/controllers/EmojiController.php
@@ -6,7 +6,20 @@ class EmojiController extends Controller {
 	public function adminHead() {
 		$this->head ();
 	}
-	public function contentFilter($text) {
-		return emoji_unified_to_html ( $text );
+	public function contentFilter($input) {
+		$html = trim($input);
+		// Create a new DOM document
+		$dom = new DOMDocument();
+			// Parse the HTML. The @ is used to suppress any parsing errors
+			// that will be thrown if the $html string isn't valid XHTML.
+		@$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+		$xpath = new DOMXPath($dom);
+		$textnodes = $xpath->query('//text()');
+		foreach($textnodes as $node) {
+			$node->textContent = emoji_unified_to_html($node->textContent);
+		}
+
+		return emoji_unified_to_html ( $dom->saveHTML() );
 	}
 }
